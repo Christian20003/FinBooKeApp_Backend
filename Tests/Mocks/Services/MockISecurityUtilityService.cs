d7681374066083ad698ae5808543aef6ab9bbb94
Christian20003 <chrisgoel@t-online.de>
2026-02-06 11:06:51 +0100

    test: Adjusted security service mock

--- Tests/Mocks/Services/MockISecurityUtilityService.cs
+++ Tests/Mocks/Services/MockISecurityUtilityService.cs
@@ -13,6 +13,8 @@ public static class MockISecurityUtilityService
         obj.Setup(o => o.Hash(It.IsAny<string>()))
             .Returns("bf1a94dd05271d2b7eee257d9f2248bcefdca0acd5708f343f8d791d6a6044b9");
         obj.Setup(o => o.IsHash(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+        obj.Setup(o => o.GenerateUniqueId(It.IsAny<Guid>(), It.IsAny<Func<Guid, Task<bool>>>()))
+            .ReturnsAsync((Guid id, Func<Guid, Task<bool>> func) => id);
         return obj;
     }
 }
