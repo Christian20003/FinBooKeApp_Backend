24e50af21cc02fb7e3011395ea589d6fdfd5c978
Christian20003 <chrisgoel@t-online.de>
2026-05-20 17:59:42 +0200

    test: Added unit test for email template builder

---
+++ Tests/Logic/Email/EmailTemplateBuilderUnitTest.cs
@@ -0,0 +1,40 @@
+using FinBooKeAPI.Logic.Email;
+using Microsoft.Extensions.Localization;
+using Moq;
+
+namespace FinBooKeAPI.Tests.Logic.Email;
+
+public class EmailTemplateBuilderUnitTest
+{
+    private readonly EmailTemplateBuilder _builder;
+    private readonly Mock<IStringLocalizer<EmailTemplateBuilder>> _localizer;
+
+    public EmailTemplateBuilderUnitTest()
+    {
+        _localizer = new Mock<IStringLocalizer<EmailTemplateBuilder>>();
+        _localizer
+            .Setup(obj => obj[It.IsAny<string>()])
+            .Returns((string key) => new LocalizedString(key, string.Empty));
+
+        _builder = new EmailTemplateBuilder(_localizer.Object);
+    }
+
+    [Fact]
+    public void Should_RemoveAllTemplateParameters_InResetPasswordTemplate()
+    {
+        var result = _builder.GetResetPasswordTemplate("link");
+
+        var hasBrackets = result.Contains("{{") || result.Contains("}}");
+        Assert.False(hasBrackets);
+    }
+
+    [Fact]
+    public void Should_AddLinkToResetPasswordTemplate()
+    {
+        var link = "http://example.com";
+        var result = _builder.GetResetPasswordTemplate(link);
+
+        var hasLink = result.Contains(link);
+        Assert.True(hasLink);
+    }
+}
