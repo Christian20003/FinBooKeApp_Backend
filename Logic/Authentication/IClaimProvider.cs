732ca2aabb8a139ba9ba95f0b9c45ea2c035b873
Christian20003 <chrisgoel@t-online.de>
2026-05-23 16:16:34 +0200

    refactor: Added comments to new interfaces

--- Logic/Authentication/IClaimProvider.cs
+++ Logic/Authentication/IClaimProvider.cs
@@ -1,11 +1,42 @@
 using System.Security.Claims;
+using FinBooKeAPI.Models.Exceptions;
 
 namespace FinBooKeAPI.Logic.Authentication;
 
+/// <summary>
+/// Handles creation of authentication token claims and extraction of claim information.
+/// </summary>
 public interface IClaimProvider
 {
+    /// <summary>
+    /// Generates a list of claims which includes the given user id and user email.
+    /// </summary>
+    /// <param name="userId">The user id</param>
+    /// <param name="email">The email address of the user</param>
+    /// <returns>A list of claims with the user id and user email</returns>
     public IEnumerable<Claim> CreateClaims(string userId, string email);
+
+    /// <summary>
+    /// Extracts the user id from a given claim identity.
+    /// </summary>
+    /// <param name="claims">The claim identity of a user</param>
+    /// <returns>The user id</returns>
+    /// <exception cref="ClaimException">If the user id claim does not exist</exception>
     public string GetUserId(ClaimsPrincipal claims);
+
+    /// <summary>
+    /// Extracts the users email address from a given claim identity.
+    /// </summary>
+    /// <param name="claims">The claim identity of a user</param>
+    /// <returns>The users email address</returns>
+    /// <exception cref="ClaimException">If the email address does not exist</exception>
     public string GetEmail(ClaimsPrincipal claims);
+
+    /// <summary>
+    /// Extracts the expiration date from a given claim identity.
+    /// </summary>
+    /// <param name="claims">The claim identity of a user</param>
+    /// <returns>The expiration date</returns>
+    /// <exception cref="ClaimException">If the expiration date does not exist</exception>
     public DateTime GetExpires(ClaimsPrincipal claims);
 }
--- Logic/Authentication/ITokenProvider.cs
+++ Logic/Authentication/ITokenProvider.cs
@@ -1,11 +1,27 @@
 using System.Security.Claims;
 using FinBooKeAPI.Models.Logic.Authentication;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FinBooKeAPI.Logic.Authentication;
 
+/// <summary>
+/// Handles the generation and verification of json web tokens (JWT).
+/// </summary>
 public interface ITokenProvider
 {
+    /// <summary>
+    /// Generates a json web token (JWT).
+    /// </summary>
+    /// <param name="payload">Payload to generate a JWT</param>
+    /// <returns>The generated JWT with its expiration time</returns>
     public AuthenticationToken CreateToken(CreateTokenPayload payload);
 
+    /// <summary>
+    /// Verifies a given json web token (JWT).
+    /// </summary>
+    /// <param name="payload">Payload to verify a JWT</param>
+    /// <returns>The claims stored inside the JWT</returns>
+    /// <exception cref="SecurityTokenMalformedException">If the given token is not a JWT</exception>
+    /// <exception cref="SecurityTokenException">If the given token is invalid</exception>
     public ClaimsPrincipal VerifyToken(VerifyTokenPayload payload);
 }
