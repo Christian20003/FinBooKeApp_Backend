bbfeba71f37df250469ce135010c8a89654d9d16
Christian20003 <chrisgoel@t-online.de>
2026-08-02 18:44:39 +0200

    refactor: Removed unecessary await statements

--- Collections/AccountCollection/AccountCollection.cs
+++ Collections/AccountCollection/AccountCollection.cs
@@ -12,54 +12,43 @@ public class AccountCollection(UserManager<UserAccount> userManager) : IAccountC
     private static readonly string LOGIN_PROVIDER = "Local";
     private static readonly string REFRESH_TOKEN_NAME = "RefreshToken";
 
-    public async Task<IdentityResult> CreateAccountAsync(UserAccount user, string password)
+    public Task<IdentityResult> CreateAccountAsync(UserAccount user, string password)
     {
-        return await _userManager.CreateAsync(user, password);
+        return _userManager.CreateAsync(user, password);
     }
 
-    public async Task<IdentityResult> DeleteAccountRefreshTokenAsync(UserAccount user)
+    public Task<IdentityResult> DeleteAccountRefreshTokenAsync(UserAccount user)
     {
-        return await _userManager.RemoveAuthenticationTokenAsync(
+        return _userManager.RemoveAuthenticationTokenAsync(
             user,
             LOGIN_PROVIDER,
             REFRESH_TOKEN_NAME
         );
     }
 
-    public async Task<string> GeneratePasswordResetTokenAsync(UserAccount user)
+    public Task<string> GeneratePasswordResetTokenAsync(UserAccount user)
     {
-        return await _userManager.GeneratePasswordResetTokenAsync(user);
+        return _userManager.GeneratePasswordResetTokenAsync(user);
     }
 
-    public async Task<UserAccount?> GetAccountAsync(Expression<Func<UserAccount, bool>> condition)
+    public Task<UserAccount?> GetAccountAsync(Expression<Func<UserAccount, bool>> condition)
     {
-        return await _userManager.Users.FirstOrDefaultAsync(condition);
+        return _userManager.Users.FirstOrDefaultAsync(condition);
     }
 
-    public async Task<string?> GetAccountRefreshTokenAsync(UserAccount user)
+    public Task<string?> GetAccountRefreshTokenAsync(UserAccount user)
     {
-        return await _userManager.GetAuthenticationTokenAsync(
-            user,
-            LOGIN_PROVIDER,
-            REFRESH_TOKEN_NAME
-        );
+        return _userManager.GetAuthenticationTokenAsync(user, LOGIN_PROVIDER, REFRESH_TOKEN_NAME);
     }
 
-    public async Task<IdentityResult> ResetPasswordAsync(
-        UserAccount user,
-        string token,
-        string password
-    )
+    public Task<IdentityResult> ResetPasswordAsync(UserAccount user, string token, string password)
     {
-        return await _userManager.ResetPasswordAsync(user, token, password);
+        return _userManager.ResetPasswordAsync(user, token, password);
     }
 
-    public async Task<IdentityResult> SetAccountRefreshTokenAsync(
-        UserAccount user,
-        string refreshToken
-    )
+    public Task<IdentityResult> SetAccountRefreshTokenAsync(UserAccount user, string refreshToken)
     {
-        return await _userManager.SetAuthenticationTokenAsync(
+        return _userManager.SetAuthenticationTokenAsync(
             user,
             LOGIN_PROVIDER,
             REFRESH_TOKEN_NAME,
@@ -67,8 +56,8 @@ public class AccountCollection(UserManager<UserAccount> userManager) : IAccountC
         );
     }
 
-    public async Task<IdentityResult> UpdateAccountAsync(UserAccount user)
+    public Task<IdentityResult> UpdateAccountAsync(UserAccount user)
     {
-        return await _userManager.UpdateAsync(user);
+        return _userManager.UpdateAsync(user);
     }
 }
